90b4466811c0359b895c9c90b9cab9b7ce1da82c
devDzign <chabour.mourad87@gmail.com>
2020-10-28 15:01:36 +0100

    init project with navigation

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from "./src/screens/index.screen";
+import {StyleSheet} from "react-native"
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="index">
+              <Stack.Screen
+                  name="index"
+                  component={IndexScreen}
+                  options={{
+                      title: 'Home',
+                      headerStyle: {
+                          backgroundColor: '#e1e1e1',
+                      },
+                      headerTintColor: '#000',
+                      headerTitleStyle: {
+                          fontWeight: 'bold',
+                          alignSelf:'center'
+                      },
+                  }}
+              />
+
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
+
+
+
+
+
+
+
