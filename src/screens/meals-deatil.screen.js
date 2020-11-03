96bb3d1f5a1b2b3f28364b8ca6c0290057860cce
devDzign <chabour.mourad87@gmail.com>
2020-11-03 14:04:11 +0100

    screen meal details

--- src/screens/meals-deatil.screen.js
+++ src/screens/meals-deatil.screen.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { ScrollView, Image, Text, View, StyleSheet, Button } from 'react-native';
 
 import { MEALS } from "../utils/dummy-data";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import HeaderButton from '../components/header-button.component'
+import HeaderButton from '../components/header-button.component';
+
+const ListItem = props => {
+    return <View style={styles.listItem}>
+        <Text style={styles.titleItem}>{props.children}</Text>
+    </View>
+}
 
 /**
  *
@@ -37,9 +43,7 @@ const MealsDetailScreen = ({navigation, route}) => {
                     </HeaderButtons>
                 ),
             });
-    },[route,meal]);
-
-
+    }, [route, meal]);
 
 
     if (!meal) {
@@ -47,23 +51,53 @@ const MealsDetailScreen = ({navigation, route}) => {
     }
 
     return (
-        <View style={styles.container}>
-            <Text>{meal.title}</Text>
-            <Button
-                title="Go Back to Categories"
-                onPress={() => {
-                    navigation.popToTop();
-                }}
-            />
-        </View>
+        <ScrollView>
+            <Image source={{uri: meal.imageUrl}} style={styles.image}/>
+            <View style={styles.details}>
+                <Text>{meal.duration}m</Text>
+                <Text>{meal.complexity}</Text>
+                <Text>{meal.affordability}</Text>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {meal.ingredients.map((ingredient,index) => {
+                return <ListItem key={index}>{ingredient}</ListItem>
+            })}
+            <Text style={styles.title}>Steps</Text>
+            {meal.steps.map((step, index) => {
+                return <ListItem key={index}>{step}</ListItem>
+            })}
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10,
+
+    },
+    titleItem:{
+        fontFamily: 'open-sans',
+        fontSize: 18,
+
     }
 });
 
